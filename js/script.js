6f2a627fb6e7608bb141c5d6d3406977b8916443
antonionovelli92 <anto.novelli@hotmail.it>
2022-12-02 15:09:40 +0100

    add traccia

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log('JS OK')
+
+/* TRACCIA: 
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 
+/*
