ca30ae2590cd2b22442855ac3e5776685490decb
antonionovelli92 <anto.novelli@hotmail.it>
2022-12-02 15:17:00 +0100

    add step

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21 
 /*
+
+/************* STEP:
+ 1. Prendo l'eleento dal DOM
+ 2. Chiede all'utente il suo nome;
+ 3. Chiede all'utente il suo cognome;
+ 4. Chiede all'utente il suo colore preferito;
+ 5. Inserisco tutti i suoi dati (NOME, COGNOME, COLORE PREFRITO) accompagnato dal numero 21;
+ ********************/
+
